093b7458ec13327519b24969de53b92a224f42fe
Sumanth Ratna <sumanth@chai.org>
2025-03-01 17:49:13 -0500

    Use Read the Docs Sphinx Theme

--- responsible-ai-content/conf.py
+++ responsible-ai-content/conf.py
@@ -13,7 +13,7 @@ author = 'Coalition for Health AI'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.mathjax']
+extensions = ['sphinx.ext.mathjax', 'sphinx_rtd_theme']
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -23,5 +23,6 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+html_logo = "_static/logo.png"
